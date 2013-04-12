61794ddc87014bc5a63f4a12fd7cbe2484e821c5
Zoran Rasic <zokiupwork@gmail.com>
2013-04-12 18:29:35 +0200

    install script

--- install-me.sh
+++ install-me.sh
@@ -1,1 +1,4 @@
-cabal-meta --dev install --constraint=clientsession==0.8.* --constraint=shakespeare==1.0.2 --constraint=syb==0.3.* --flags=blaze_html_0_5
+cabal-meta --dev install --constraint=clientsession==0.8.* \
+                         --constraint=shakespeare==1.0.2   \
+                         --constraint=syb==0.3.*           \
+                         --flags=blaze_html_0_5
